09bb8d17df0cc5b83c8934fa349cc6390131e5ea
Jeyavani2 <mahes_75@yahoo.com>
2025-06-30 01:29:24 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -13,7 +13,7 @@ st.title('image generator')
 pr=st.text_input("Enter prompt")
 
 if pr is not None and st.button("generate"):
-    client=genai.Client(api_key='AIzaSyDdDYoT8dGRfNpXRUAHrQMHHXUPOu-Hhpk')
+    client=genai.Client(api_key='your api key')
     response=client.models.generate_images(
         model='imagen-3.0-generate-002',
         prompt=pr,
